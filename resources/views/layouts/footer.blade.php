ff8984f4144ec6e0b9e58a5530c487c1a7d5653a
syaifulkiara <syaiful.kiara.com>
2023-08-18 00:25:24 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Inventory PLUS © {{ date('Y') }} || Developed by <strong><a target="_blank" href="">Dalusy</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.1.0</strong></div>
+</footer>
